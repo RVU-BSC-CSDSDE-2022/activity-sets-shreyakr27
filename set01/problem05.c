616716377ec1a202ecbec6981a71a308e1066ddb
shreyakr27 <114557811+shreyakr27@users.noreply.github.com>
2022-12-07 06:55:21 +0000

    solved

--- set01/problem05.c
+++ set01/problem05.c
@@ -3,36 +3,43 @@ int input();
 int compare(int a, int b, int c);
 void output(int a, int b, int c, int largest);
 
-
-int main(){
-  int a,b,c,largest;
-  a=input();
-  b=input();
-  c=input();
-  largest=compare(a,b,c);
-  output(a,b,c,largest);
-  
+int input()
+{
+  int num;
+  printf("enter num: ");
+  scanf("%d", &num);
+  return num;
 }
 
-
-
-int input(){
-  int n;
-  printf("enter the value");
-  scanf("%d", &n);
-  return n;
+int compare(int a, int b, int c)
+{
+  int largest;
+  if(a>=b && a>=c)
+  {
+    largest = a;
+  }
+  if(b>=a && b>=c)
+  {
+    largest = b;
+  }
+  if(c>=a && c>=b)
+  {
+    largest = c;
+  }
+  return largest;
 }
 
-int compare(int a, int b, int c){
-  if(a>=b && a>=c){
-    printf("The largest of %d,%d,%d is %d ",a,b,c,a);
-    } 
-  if(b>=a && b>=c){
-    printf("The largest of %d,%d,%d is %d ",a,b,c,b);
-    }  
-  if(c>=a && c>=b){
-    printf("The largest of %d,%d,%d is %d ",a,b,c,c);
-    }
-void output(int a, int b, int c, int largest){
-  printf("");
+void output(int a, int b, int c, int largest)
+{
+  printf("the largest of %d, %d and %d", a, b, c, largest);
+}
+
+int main()
+{
+  int a, b, c, largest;
+  a = input();
+  b = input();
+  c = input();
+  largest = compare(a, b, c);
+  output(a, b, c, largest);
 }
